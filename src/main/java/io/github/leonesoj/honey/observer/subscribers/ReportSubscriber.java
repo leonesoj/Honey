fea7f602a7894b2f5eadd4fb4edad31b89b9fdd8
Jose Leon <161084115+leonesoj@users.noreply.github.com>
2025-08-10 14:58:06 -0700

    feat: notify the report issuer when their report becomes resolved (#11)

--- src/main/java/io/github/leonesoj/honey/observer/subscribers/ReportSubscriber.java
+++ src/main/java/io/github/leonesoj/honey/observer/subscribers/ReportSubscriber.java
@@ -4,12 +4,15 @@ import static io.github.leonesoj.honey.locale.Message.argComponent;
 
 import io.github.leonesoj.honey.Honey;
 import io.github.leonesoj.honey.database.data.model.Report;
+import io.github.leonesoj.honey.database.data.model.Report.ReportStatus;
 import io.github.leonesoj.honey.observer.Observer;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.event.ClickEvent;
 import net.kyori.adventure.text.minimessage.tag.resolver.Placeholder;
 import net.kyori.adventure.text.minimessage.translation.Argument;
 import org.bukkit.Bukkit;
+import org.bukkit.Sound;
+import org.bukkit.entity.Player;
 
 public class ReportSubscriber implements Observer<Report> {
 
@@ -35,5 +38,16 @@ public class ReportSubscriber implements Observer<Report> {
             }));
       }
     });
+  }
+
+  @Override
+  public void onUpdate(Report report) {
+    if (report.getStatus().equals(ReportStatus.RESOLVED)) {
+      Player issuer = Bukkit.getPlayer(report.getIssuer());
+      if (issuer != null) {
+        issuer.sendMessage(Component.translatable("honey.report.resolved"));
+        issuer.playSound(issuer, Sound.ENTITY_ARROW_HIT_PLAYER, 1F, 0.5F);
+      }
+    }
   }
 }
