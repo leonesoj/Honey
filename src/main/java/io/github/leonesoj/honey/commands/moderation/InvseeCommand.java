c953d0ad079e4bc2ce8d11d9a4042adf9c06db95
Jose Leon <161084115+leonesoj@users.noreply.github.com>
2025-10-21 18:40:08 -0700

    chore: cleanup dead code/functionality (#50)

--- src/main/java/io/github/leonesoj/honey/commands/moderation/InvseeCommand.java
+++ src/main/java/io/github/leonesoj/honey/commands/moderation/InvseeCommand.java
@@ -2,13 +2,10 @@ package io.github.leonesoj.honey.commands.moderation;
 
 import com.mojang.brigadier.Command;
 import com.mojang.brigadier.context.CommandContext;
-import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import com.mojang.brigadier.tree.LiteralCommandNode;
 import io.github.leonesoj.honey.utils.command.OtherPlayerArgument;
 import io.papermc.paper.command.brigadier.CommandSourceStack;
 import io.papermc.paper.command.brigadier.Commands;
-import io.papermc.paper.command.brigadier.argument.ArgumentTypes;
-import io.papermc.paper.command.brigadier.argument.resolvers.selector.PlayerSelectorArgumentResolver;
 import org.bukkit.entity.Player;
 
 public class InvseeCommand {
